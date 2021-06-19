bc2a4cb62ad7df19ff39b2808c4f2eba13040be8
Yilun Xiao <yilunxiao2@gmail.com>
2021-06-18 23:05:11 -0400

    modified resolver.js in server

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// Get both typedefs and resolvers and export them together
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
