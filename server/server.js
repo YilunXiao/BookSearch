28fdd948cb1cf0b452829257204ee411d00907fe
Yilun Xiao <yilunxiao2@gmail.com>
2021-06-18 22:30:27 -0400

    created schemas folder with resolver and typedefs files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+// Require apollo-server-express
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
+
+// get typedef and resolver files
+const { typeDefs, resolvers } = require('./schemas');
+// get middleware for authorizations
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
+
+// ROUTES NOT NEEDED WITH APOLLO SERVER
 const routes = require('./routes');
 
 const app = express();
